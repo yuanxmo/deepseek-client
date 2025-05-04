c30e3b99a7cf8f87ce38bacc625de000f81d78cd
YuanxMo <1764187345@qq.com>
2025-05-04 21:34:16 +0800

    add deepseek client

---
+++ src/main/java/com/yuanxmo/deepseek/client/DeepseekClient.java
@@ -0,0 +1,64 @@
+package com.yuanxmo.deepseek.client;
+
+import com.google.gson.Gson;
+import com.yuanxmo.deepseek.domain.request.DeepseekRequest;
+import com.yuanxmo.deepseek.domain.request.DeepseekRequestMessage;
+import com.yuanxmo.deepseek.domain.response.DeepseekResponse;
+import com.yuanxmo.deepseek.properties.DeepseekProperties;
+import com.yuanxmo.deepseek.utils.DeepseekModelUtils;
+import com.yuanxmo.deepseek.utils.DeepseekUrlUtils;
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class DeepseekClient {
+
+    private DeepseekProperties properties;
+    private DeepseekRequestMessage systemMessage;
+    private OkHttpClient client = new OkHttpClient();
+
+    public DeepseekClient(DeepseekProperties properties) {
+        this.properties = properties;
+    }
+
+    public DeepseekClient(DeepseekProperties properties, DeepseekRequestMessage systemMessage) {
+        this.properties = properties;
+    }
+
+    private Request getRequest(DeepseekRequest request) {
+        return new Request.Builder()
+                .url(DeepseekUrlUtils.getUrl(properties.getMode()))
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + properties.getAppKey())
+                .post(RequestBody.create(request.toJson(), MediaType.parse("application/json")))
+                .build();
+    }
+
+    private DeepseekRequest getDeepseekRequest(String msg) {
+        DeepseekRequest deepseekRequest = new DeepseekRequest(
+                DeepseekModelUtils.getDeepseekModel(properties.getModel()),
+                systemMessage,
+                msg
+        );
+        return deepseekRequest;
+    }
+
+    public DeepseekResponse newCall(String msg) {
+        DeepseekRequest deepseekRequest = getDeepseekRequest(msg);
+        Request request = getRequest(deepseekRequest);
+        Response response;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Gson gson = new Gson();
+        DeepseekResponse deepseekResponse = gson.fromJson(response.body().charStream(), DeepseekResponse.class);
+        deepseekResponse.getChoices().stream().forEach(choice -> {
+            choice.getMessage().setContent(
+                    choice.getMessage().getContent().replace("```json", "").replace("```", "").trim()
+            );
+        });
+        return deepseekResponse;
+    }
+}
